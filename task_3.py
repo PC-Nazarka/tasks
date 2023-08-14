dbc40f4d2f3c684440151c8355be6c9917a6e368
Nazar <furnoxl@yandex.ru>
2023-08-14 23:18:36 +0700

    Thrid task

---
+++ task_3.py
@@ -0,0 +1,8 @@
+nums = [7, 1, 3, 2, 8]
+for i in range(len(nums)):
+    for j in range(i):
+        if nums[i] < nums[j]:
+            item = nums.pop(i)
+            nums.insert(j, item)
+print(nums)
+            
